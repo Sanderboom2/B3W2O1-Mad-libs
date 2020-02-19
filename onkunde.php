acbaeb8c308a2d5faa84ab6a4351213c9788934d
Sander <99058223@mydavinci.nl>
2020-02-19 12:18:34 +0100

    naar huis

---
+++ onkunde.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madlibs - Onkunde</title>
+    <link rel="stylesheet" type="text/css" href="madlibs.css">
+</head>
+<body>
+    <div id='container'>
+        <h1 id='title'>Mad libs</h1>
+        <div id='menu'>
+            <ul>
+                <li><a href='paniek.php'>Er heerst paniek...</a></li>
+                <li><a href='onkunde.php'>Onkunde</a></li>
+            </ul>
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ paniek.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madlibs - Er heerst paniek...</title>
+    <link rel="stylesheet" type="text/css" href="madlibs.css">
+</head>
+<body>
+    <div id='container'>
+        <h1 id='title'>Mad libs</h1>
+        <div id='menu'>
+            <ul>
+                <li><a href='paniek.php'>Er heerst paniek...</a></li>
+                <li><a href='onkunde.php'>Onkunde</a></li>
+            </ul>
+        </div>
+        <div id='page'>
+            <h1>Er heerst paniek...</h1>
+            <form>
+            Welk dier zou je nooit als huisdier willen hebben? <input type='text' name='paniek1'><br><br>
+            Wie is de belangrijkste persoon in je leven? <input type='text' name='paniek2'><br><br>
+            In welk land zou je graag willen wonen? <input type='text' name='paniek3'><br><br>
+            Wat doe je als je je verveelt? <input type='text' name='paniek4'><br><br>
+            Met welk speelgoed speelde je als kind het meest? <input type='text' name='paniek5'><br><br>
+            Bij welke docent spijbel je het liefts? <input type='text' name='paniek6'><br><br>
+            Als je €100.00,- had, wat zou je dan kopen? <input type='text' name='paniek7'><br><br>
+            Wat is je favoriete bezigheid? <input type='text'  name='paniek8'><br><br>
+            <input type='submit' id='submit'>
+            </form>
+        </div>
+        <div id='footer'>
+            <p>Sander Boom ©2020</p>
+        </div>
+    </div>
+</body>
+</html>
